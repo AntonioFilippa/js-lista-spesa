8e0829b5812baa0a63420609d3b3fa4f91e37242
antoniofilippa94@gmail.com <antoniofilippa94@gmail.com>
2024-05-06 15:14:19 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const listaSpesa = [
+    "Latte",
+    "Pane",
+    "Uova",
+    "Frutta",
+    "Verdura",
+    "Carne",
+    "Pesce",
+    "Pasta",
+    "Riso",
+    "Formaggio"
+  ];
+  
+  const listaElementi = document.getElementById("lista-spesa");
+  
+  let i = 0;
+
+  while(i < listaSpesa.length){
+    const nuovoElemento = document.createElement("li");
+    nuovoElemento.innerHTML = listaSpesa[i];
+    listaElementi.appendChild(nuovoElemento);
+    i++;
+  }
+
+  
